321d7fc839bfae42718bf14f6fb5f136431f8812
minsoyoung <thdud6830@gmail.com>
2016-07-22 17:53:51 +0900

    js

---
+++ class/webpage/js/gnb.js
@@ -0,0 +1,19 @@
+/**
+ * Created by 321-08 on 2016-07-22.
+ */
+
+
+
+$(function(){
+
+    $('.gnb-depth1-link').on('mouseenter', function() {
+        $('.gnb-depth2').show();
+        $('.gnb-bg').show();
+    });
+
+    $('.gnb-depth1-link').on('mouseleave', function() {
+        $('.gnb-depth2').hide();
+        $('.gnb-bg').hide();
+    });
+
+});
