54794bccd30a0da8a33fdbb5c050ceeaffa93af3
minsoyoung <thdud6830@gmail.com>
2016-07-25 17:57:48 +0900

    no message

--- class/webpage/js/gnb.js
+++ class/webpage/js/gnb.js
@@ -7,13 +7,17 @@
 $(function(){
 
     $('.gnb-depth1-link').on('mouseenter', function() {
-        $('.gnb-depth2').show();
-        $('.gnb-bg').show();
+        $('.gnb-depth2').delay(500).stop().fadeIn(1000);
+        $('.gnb-bg').stop().animate({
+            height:274
+        });
     });
 
     $('.gnb-depth1-link').on('mouseleave', function() {
-        $('.gnb-depth2').hide();
-        $('.gnb-bg').hide();
+        $('.gnb-depth2').stop().fadeOut(30);
+        $('.gnb-bg').stop().animate({
+            height:0
+        });
     });
 
 });
